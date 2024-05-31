15f727d5400ca3c1e424ff4432d67368fff610c8
Oleg Kuzmenko <okuzmenko31us@gmail.com>
2024-06-01 02:16:15 +0300

    Create config and added some helpers for it

---
+++ api/src/core/config.py
@@ -0,0 +1,73 @@
+from functools import lru_cache
+
+from pydantic import (
+    Field,
+    field_validator,
+    ValidationInfo,
+    PostgresDsn,
+)
+from pydantic_settings import BaseSettings
+
+from .helpers import DotenvListHelper, load_environment
+
+
+class CorsSettings(BaseSettings):
+    origins: str = Field(alias="cors_origins")
+
+    @field_validator("origins")
+    @classmethod
+    def assemble_cors_origins(cls, v: str) -> list[str]:
+        return DotenvListHelper.get_list_from_value(v)
+
+
+class DBSettings(BaseSettings):
+    name: str = Field(alias="db_name")
+    user: str = Field(alias="db_user")
+    password: str = Field(alias="db_password")
+    host: str = Field(alias="db_host")
+    port: int = Field(alias="db_port")
+    scheme: str = Field(alias="db_scheme")
+    url: str | None = Field(alias="db_url", default=None)
+
+    @field_validator("url")
+    @classmethod
+    def assemble_db_url(
+        cls, v: str | None, validation_info: ValidationInfo
+    ) -> str:
+        if v is not None:
+            return v
+        values = validation_info.data
+        url = PostgresDsn.build(
+            scheme=values["scheme"],
+            username=values["user"],
+            password=values["password"],
+            host=values["host"],
+            port=values["port"],
+            path=values["name"],
+        )
+        return str(url)
+
+
+class Settings(BaseSettings):
+    # App settings
+    app_name: str = "Relict Arte API"
+    app_version: int = 1
+    app_domain: str = "localhost:8000"
+    app_scheme: str = "http"
+    debug: bool = True
+    secret_key: str
+
+    # Cors
+    cors: CorsSettings = Field(default_factory=CorsSettings)
+
+    # Database settings
+    db: DBSettings = Field(default_factory=DBSettings)
+
+
+@lru_cache
+def get_settings() -> Settings:
+    load_environment()
+    return Settings()
+
+
+settings = get_settings()
