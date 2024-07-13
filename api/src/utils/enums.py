bff0c46116d4d93865abbb49726adc1346f8c6b1
Oleg Kuzmenko <okuzmenko31us@gmail.com>
2024-07-13 13:08:41 +0300

    Added models for product and related with it objects

--- api/src/utils/enums.py
+++ api/src/utils/enums.py
@@ -4,9 +4,12 @@ from enum import Enum
 
 
 class BaseEnum(Enum):
-    def __eq__(self, value: Any) -> bool:
-        return value == self.value
-
     @classmethod
     def values(cls):
         return list(map(lambda c: c.value, cls))
+
+    def __eq__(self, value: Any) -> bool:
+        return value == self.value
+
+    def __hash__(self) -> int:
+        return super().__hash__()
