499a8c259a8f8cb09d0277118535974b104fcdd0
Vladyslav Malyshenko <vmalyshenko32@gmail.com>
2024-07-14 16:22:52 +0300

    Fixed some bugs

--- admin/src/components/ActionModal.tsx
+++ admin/src/components/ActionModal.tsx
@@ -25,11 +25,34 @@ const ActionModal = () => {
             {action !== "" && (
                 <div className="action-modal-container" onClick={closeModal}>
                     <div
-                        className="action-modal"
+                        className={`action-modal${
+                            action === "delete" ? " delete" : ""
+                        }`}
                         onClick={(e) => e.stopPropagation()}
                     >
                         <div className="action-modal-content">
-                            {action !== "add" ? (
+                            {action === "delete" ? (
+                                <>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                                        fill="currentColor"
+                                        version="1.1"
+                                        id="Layer_1"
+                                        viewBox="0 0 511.999 511.999"
+                                        xmlSpace="preserve"
+                                    >
+                                        <path d="M506.43,421.536L291.573,49.394c-15.814-27.391-55.327-27.401-71.147,0L5.568,421.536    c-15.814,27.391,3.934,61.616,35.574,61.616h429.714C502.485,483.153,522.25,448.938,506.43,421.536z M274.821,385.034    c0,10.394-8.427,18.821-18.821,18.821s-18.821-8.427-18.821-18.821v-11.239c0-10.394,8.427-18.821,18.821-18.821    s18.821,8.427,18.821,18.821V385.034z M274.821,311.702c0,10.394-8.427,18.821-18.821,18.821s-18.821-8.427-18.821-18.821v-107.89    c0-10.394,8.427-18.821,18.821-18.821s18.821,8.427,18.821,18.821V311.702z" />
+                                    </svg>
+
+                                    <p>
+                                        При видаленні об'єкту з номером{" "}
+                                        {item.id}, його не можна буде повернути.
+                                        Ці дії НЕ ЗВОРОТНІ!
+                                    </p>
+                                    <p>Ви дійсно хочете видалити {item.id}?</p>
+                                </>
+                            ) : action !== "add" ? (
                                 <>
                                     {category.inputFields.length > 0 ? (
                                         <>
@@ -135,51 +158,6 @@ const ActionModal = () => {
                                                     </label>
                                                 )
                                             )}
-
-                                            <div className="action-modal-buttons">
-                                                {action !== "show" && (
-                                                    <button
-                                                        onClick={() => {
-                                                            closeModal();
-                                                        }}
-                                                    >
-                                                        Close
-                                                    </button>
-                                                )}
-
-                                                <button
-                                                    className={action}
-                                                    onClick={() => {
-                                                        if (action === "show") {
-                                                            closeModal();
-                                                        } else if (
-                                                            action === "edit"
-                                                        ) {
-                                                            editItem(
-                                                                category.editUrl,
-                                                                {
-                                                                    ...item,
-                                                                    id: undefined,
-                                                                },
-                                                                { id: item.id }
-                                                            );
-                                                        } else {
-                                                            deleteItem(
-                                                                category.deleteUrl,
-                                                                {
-                                                                    id: item.id,
-                                                                }
-                                                            );
-                                                        }
-                                                    }}
-                                                >
-                                                    {action === "edit"
-                                                        ? "Save"
-                                                        : action === "show"
-                                                        ? "Close"
-                                                        : "Delete"}
-                                                </button>
-                                            </div>
                                         </>
                                     ) : (
                                         <Loader />
@@ -249,51 +227,100 @@ const ActionModal = () => {
                                                 </label>
                                             )
                                         )}
-                                    <div className="action-modal-buttons">
-                                        <button
-                                            onClick={() => {
-                                                closeModal();
-                                            }}
-                                        >
-                                            Close
-                                        </button>
-                                        <button
-                                            className={action}
-                                            onClick={() => {
-                                                let newItem = item;
-
-                                                category.addItemFields.forEach(
-                                                    (itemField: any) => {
-                                                        if (
-                                                            !newItem[
-                                                                itemField.field_name ||
-                                                                    itemField.name
-                                                            ]
-                                                        ) {
-                                                            newItem[
-                                                                itemField.field_name ||
-                                                                    itemField.name
-                                                            ] =
-                                                                itemField.type ===
-                                                                    "checkbox" ||
-                                                                itemField.type ===
-                                                                    "radio"
-                                                                    ? false
-                                                                    : "";
-                                                        }
-                                                    }
-                                                );
-                                                addItem(
-                                                    category.addUrl,
-                                                    newItem
-                                                );
-                                            }}
-                                        >
-                                            Add
-                                        </button>
-                                    </div>
                                 </>
                             )}
+
+                            <div className="action-modal-buttons">
+                                {action !== "show" && (
+                                    <button
+                                        onClick={() => {
+                                            closeModal();
+                                        }}
+                                    >
+                                        Close
+                                    </button>
+                                )}
+
+                                <button
+                                    className={action}
+                                    onClick={() => {
+                                        if (action === "show") {
+                                            closeModal();
+                                        } else if (action === "edit") {
+                                            let newItem = { ...item };
+
+                                            Object.keys(newItem).forEach(
+                                                (itemKey) => {
+                                                    if (
+                                                        newItem[itemKey] &&
+                                                        !category.addItemFields.some(
+                                                            (field: any) =>
+                                                                (field.field_name ||
+                                                                    field.name) ===
+                                                                itemKey
+                                                        )
+                                                    ) {
+                                                        delete newItem[itemKey];
+                                                    }
+                                                }
+                                            );
+
+                                            editItem(
+                                                category.editUrl,
+                                                newItem,
+                                                { id: item.id }
+                                            );
+                                        } else if (action === "delete") {
+                                            deleteItem(category.deleteUrl, {
+                                                id: item.id,
+                                            });
+                                        } else if (action === "add") {
+                                            let newItem = { ...item };
+
+                                            Object.keys(newItem).forEach(
+                                                (itemKey) => {
+                                                    if (!newItem[itemKey]) {
+                                                        const fieldType =
+                                                            category.addItemFields.find(
+                                                                (field: any) =>
+                                                                    (field.field_name ||
+                                                                        field.name) ===
+                                                                    itemKey
+                                                            ).type;
+                                                        newItem[itemKey] =
+                                                            fieldType ===
+                                                                "checkbox" ||
+                                                            fieldType ===
+                                                                "radio"
+                                                                ? false
+                                                                : "";
+                                                    } else if (
+                                                        newItem[itemKey] &&
+                                                        !category.addItemFields.some(
+                                                            (field: any) =>
+                                                                (field.field_name ||
+                                                                    field.name) ===
+                                                                itemKey
+                                                        )
+                                                    ) {
+                                                        delete newItem[itemKey];
+                                                    }
+                                                }
+                                            );
+
+                                            addItem(category.addUrl, newItem);
+                                        }
+                                    }}
+                                >
+                                    {action === "edit"
+                                        ? "Save"
+                                        : action === "show"
+                                        ? "Close"
+                                        : action === "delete"
+                                        ? "Delete"
+                                        : "Add"}
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
