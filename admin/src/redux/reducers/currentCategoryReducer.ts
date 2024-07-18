c25d706671f127b5bfab156054daae6b1f1101e9
Vladyslav Malyshenko <vmalyshenko32@gmail.com>
2024-07-18 20:49:54 +0300

    Made some types for more improvement

--- admin/src/redux/reducers/currentCategoryReducer.ts
+++ admin/src/redux/reducers/currentCategoryReducer.ts
@@ -1,26 +1,27 @@
 import { categoriesData } from "../../data/categories";
+import { Category, MainCategory } from "../../types/categoriesTypes";
 import { SET_CURRENT_CATEGORY } from "../actionTypes/currentCategoryActionTypes";
 
 const initialState = {
-    category:
-        categoriesData.find(
-            (category: any) =>
-                (category.link
-                    .split("/")
-                    .filter((part: string) => part.trim() !== "")[0] || "/") ===
-                (window.location.href
-                    .split("/")
-                    .filter((part: string) => part.trim() !== "")[2] || "/")
-        ) || categoriesData[0],
+  category:
+    categoriesData.find(
+      (category: Category | MainCategory) =>
+        (category.link
+          .split("/")
+          .filter((part: string) => part.trim() !== "")[0] || "/") ===
+        (window.location.href
+          .split("/")
+          .filter((part: string) => part.trim() !== "")[2] || "/")
+    ) || categoriesData[0],
 };
 
 export const currentCategoryReducer = (state = initialState, action: any) => {
-    switch (action.type) {
-        case SET_CURRENT_CATEGORY:
-            return {
-                category: action.payload,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_CURRENT_CATEGORY:
+      return {
+        category: action.payload,
+      };
+    default:
+      return state;
+  }
 };
