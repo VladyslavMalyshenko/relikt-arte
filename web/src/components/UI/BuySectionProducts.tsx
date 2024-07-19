b93acbddacf6c740f3e50350f4bcef42d97cd5f0
Vladyslav Malyshenko <vmalyshenko32@gmail.com>
2024-07-19 19:32:54 +0300

    Made initial steps to connect backend

--- web/src/components/UI/BuySectionProducts.tsx
+++ web/src/components/UI/BuySectionProducts.tsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from "react";
-import { ProductType, productsData } from "../../data/products";
 import "../../styles/components/UI/BuySectionProducts.scss";
+import { ProductType } from "../../types/productsRelatedTypes";
+import { getItems } from "../../utils/getItems";
 import BuyProductsPagination from "./BuyProductsPagination";
 import DoorCard from "./DoorCard";
 
 const BuySectionProducts = () => {
     const [currentPage, setCurrentPage] = useState(1);
+    const [products, setProduts] = useState<ProductType[]>([]);
     const pages = 4;
 
     useEffect(() => {
+        let page = currentPage;
         if (currentPage < 1) {
             setCurrentPage(1);
+            page = 1;
         }
+
+        const getProducts = async () => {
+            const newProducts = await getItems(
+                `/api/v1/product/list?page=${page}`
+            );
+            setProduts(newProducts || []);
+        };
+
+        getProducts();
     }, [currentPage]);
 
     const changePage = (page: number) => {
@@ -21,8 +34,8 @@ const BuySectionProducts = () => {
     return (
         <div className="buy-products">
             <div className="buy-products-wrapper">
-                {productsData &&
-                    productsData.map((product: ProductType, index) => (
+                {products &&
+                    products.map((product: ProductType, index) => (
                         <DoorCard key={`product[${index}]`} product={product} />
                     ))}
             </div>
