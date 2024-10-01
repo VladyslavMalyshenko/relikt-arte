ead5b591287eded9a0db3315cdc8af5e2f277e3e
Oleg Kuzmenko <okuzmenko31us@gmail.com>
2024-10-01 13:22:24 +0300

    email fix

--- api/src/user/utils.py
+++ api/src/user/utils.py
@@ -55,17 +55,17 @@ class AuthTokenEmailManager(BaseEmailManager):
     async def send_email_change_confirmation(self, token_data: AuthTokenShow):
         body_message = f"""
             <div style="line-height:1.5;">
-                <p style="margin: 0;">Вітаємо!</p>
+                <p style="margin: 0;">👋 Вітаємо!</p>
                 <p style="margin: 0;">Ми отримали запит на зміну електронної пошти для вашого облікового запису на платформі Relict Arte.</p>
                 <p style="margin: 0;">Щоб підтвердити зміну електронної пошти, будь ласка, натисніть на посилання нижче:</p>
-                <p style="margin: 0;">
+                <p style="text-align: center; margin: 20px 0;">
                     <a href="{settings.frontend_app.base_url}/{settings.frontend_app.email_change_confirm_path}/{token_data.token}"
                     style="background-color:#007bff;color:#ffffff;padding:10px 20px;border-radius:5px;text-decoration:none;">
-                    Підтвердити зміну електронної пошти
+                    Підтвердити зміну електронної пошти ✉️
                     </a>
                 </p>
                 <p style="margin: 0;">Якщо ви не ініціювали цей запит, просто проігноруйте це повідомлення.</p>
-                <p style="margin: 0;">З повагою, команда Relict Arte</p>
+                <p style="margin: 0;">З повагою, команда Relict Arte 😊</p>
             </div>
         """
         context = AuthTokenEmailMessageContext(
